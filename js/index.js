8d1f6f62cf1e50a49fd9e290f702969be365c341
amazingHanim <chee_up_8282@naver.com>
2022-01-09 15:55:52 +0900

    요요
    요요

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+ //heart 아이콘 클릭 이벤트
+
+
+var $product = $(".products");
+
+
+var $emptyHeart = $product.find('.emptyHeart'),
+$fullHeart = $product.find('.fullHeart');
+
+$fullHeart.hide();
+$emptyHeart.click(function(){
+    $(this).toggle();
+    $(this).next().toggle();
+})
+$fullHeart.click(function(){
+    $(this).toggle();
+    $(this).prev().toggle();
+})   
+})
