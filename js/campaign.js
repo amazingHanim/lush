c903a5b708559ce9a3266998329eb0bcdbe2f594
amazingHanim <chee_up_8282@naver.com>
2022-01-16 17:27:12 +0900

    ss
    ss

---
+++ js/campaign.js
@@ -0,0 +1,19 @@
+$(function () {
+
+
+    var $campaign = $(".campaign"),
+        $fixed = $campaign.children(".fixed"),
+        $header = $campaign.prev().find('header')
+
+
+
+
+    $header_height = $header.innerHeight();
+    $fixed_height = $(window).innerHeight() - $header_height + 'px';
+    $fixed.css(
+        'height', $fixed_height);
+    $fixed.css(
+        'top',$header_height+"px"
+    )
+
+})
