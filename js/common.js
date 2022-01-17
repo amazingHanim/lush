a2ac7126eee79ed2f088e158eb3bd6a8f003f5b5
amazingHanim <chee_up_8282@naver.com>
2022-01-17 15:31:29 +0900

    중 저
    할아버지~

--- js/common.js
+++ js/common.js
@@ -1,20 +1,20 @@
-$(function(){
- //heart 아이콘 클릭 이벤트
+$(function () {
+    //heart 아이콘 클릭 이벤트
 
 
-var $product = $(".products");
+    var $product = $(".products");
 
 
-var $emptyHeart = $product.find('.emptyHeart'),
-$fullHeart = $product.find('.fullHeart');
+    var $emptyHeart = $product.find('.emptyHeart'),
+        $fullHeart = $product.find('.fullHeart');
 
-$fullHeart.hide();
-$emptyHeart.click(function(){
-    $(this).toggle();
-    $(this).next().toggle();
-})
-$fullHeart.click(function(){
-    $(this).toggle();
-    $(this).prev().toggle();
-})   
+    $fullHeart.hide();
+    $emptyHeart.click(function () {
+        $(this).toggle();
+        $(this).next().toggle();
+    })
+    $fullHeart.click(function () {
+        $(this).toggle();
+        $(this).prev().toggle();
+    })
 })
