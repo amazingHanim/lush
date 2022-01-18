6017ba36e0ded49419559d3afdf356ac798e2bad
amazingHanim <chee_up_8282@naver.com>
2022-01-18 17:59:21 +0900

    sss
    sss

--- js/index.js
+++ js/index.js
@@ -13,30 +13,44 @@ $(function () {
             hovernum = $(this).index();
             imgHeignt = $imgList.innerHeight();
             $imgList.stop();
-            $imgList.animate({'top':-(hovernum * imgHeignt)},300);
+            $imgList.animate({
+                'top': -(hovernum * imgHeignt)
+            }, 300);
         })
     }
     //lips content fin
 
-    function about(){
+    function about() {
         var $about = $(".about"),
             $txt = $about.find(".txt")
-            $img = $about.find(".img")
+        $img = $about.find(".img")
 
 
-            $txt.find("span").mouseenter(
-                function(){
-                    $txt.children("li").removeClass("on");
-                    $(this).parent().addClass("on");//해당 텍스트에 컬러 번경 fin
-                    $thisNum = $(this).parent().index();
-                    $img.css({"background-image":"url(../images/about_"+ ($thisNum+1) +".jpeg)"},300)
-                }
-            )
+        $txt.find("span").mouseenter(
+            function () {
+                $txt.children("li").removeClass("on");
+                $(this).parent().addClass("on"); //해당 텍스트에 컬러 번경 fin
+                $thisNum = $(this).parent().index();
+                $img.css({
+                    "background-image": "url(../images/about_" + ($thisNum + 1) + ".jpeg)"
+                }, 300)
+            }
+        )
 
-            
+
 
     }
     //about content fin
+    function mainSlide() {
+        var $main = $(".main"),
+            $slider = $main.find(".slider");
+        $slider.slick({
+            lazyLoad: 'ondemand', // ondemand progressive anticipated
+            infinite: true
+        });
+    }
+
     listHover();
     about();
+    mainSlide();
 })
